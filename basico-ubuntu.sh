4d28637b029aad5341944c05abcdf1404ebec444
ArmandoG <100459557+Armandogl14@users.noreply.github.com>
2024-04-14 15:20:40 -0400

    Update basico-ubuntu.sh

--- basico-ubuntu.sh
+++ basico-ubuntu.sh
@@ -44,8 +44,8 @@ cd proyecto-final-web
 
 # Ejecutando la creación de fatjar
 chmod +x gradlew
-./gradlew shadowjar
 ./gradlew generateProto
+./gradlew shadowjar
 
 # Creando variables de entorno
 export URL_MONGO="mongodb+srv://ajgl0001:o8xEX97KS8GPGnR0@cluster0.hadgter.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
